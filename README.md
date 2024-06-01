9ba6a4a8ac52811d53c09aef967e3e17c00b6085
GisoreBrian <freelancerkazungudev@gmail.com>
2024-06-01 08:19:02 +0300

    Supercharge the state-management in your Dart apps with Transparent Functional Reactive Programming (TFRP)

---
+++ README.md
@@ -0,0 +1,6 @@
+# flutter_counter_mobx_app
+
+In this tutorial you will learn how to create a MobX version 
+of the default Flutter "counter" app.
+
+
---
+++ android/app/src/main/kotlin/com/kazungudev/flutter_counter_mobx_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.kazungudev.flutter_counter_mobx_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
